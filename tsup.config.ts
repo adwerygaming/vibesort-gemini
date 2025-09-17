6032af6f47ab1757551fea298964bfc1b1d24000
MasDepan <50104357+adwerygaming@users.noreply.github.com>
2025-09-17 09:22:58 +0700

    chore: Update project name and configurations
    - chore(deps): Add package-lock.json to .gitignore
    - chore: Remove nodemon.json configuration
    - chore(deps): Update package name to vibesort-gemini
    - chore(build): Exclude example directory from tsup build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-    entry: ['src/**/*.ts', "!src/**/node_modules/**"],
+    entry: ['src/**/*.ts', "!src/**/node_modules/**", "!src/example/**"],
     outDir: 'dist',
     format: ['esm'],
     sourcemap: true,
